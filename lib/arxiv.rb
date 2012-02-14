02fc2f68353b10ac35729bff95488a206dc8e9da
Cory Schires <coryschires@gmail.com>
2012-02-14 00:00:39 -0600

    Better error handling.

--- lib/arxiv.rb
+++ lib/arxiv.rb
@@ -12,10 +12,22 @@ require 'arxiv/models/manuscript'
 
 module Arxiv
 
+  module Error
+    class ManuscriptNotFound < StandardError ; end
+    class MalformedId < StandardError ; end
+  end
+
+  ID_FORMAT = /^\d{4}\.\d{4}(?:v\d+)?$/
+
   def self.get(id)
+    raise Arxiv::Error::MalformedId, "Manuscript ID format is invalid" unless id =~ ID_FORMAT
+
     url = ::URI.parse("http://export.arxiv.org/api/query?id_list=#{id}")
     response = ::Nokogiri::XML(open(url)).remove_namespaces!
     manuscript = Arxiv::Manuscript.parse(response.to_s, single: id)
+
+    raise Arxiv::Error::ManuscriptNotFound, "Manuscript #{id} doesn't exist on arXiv" if manuscript.title.nil?
+    manuscript
   end
 
 end
---
+++ spec/arxiv/arxiv_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module Arxiv
+
+  RSpec::Matchers.define :fetch_valid_manuscript do |expected|
+    match do |actual|
+      expected_title = "Laser frequency comb techniques for precise astronomical spectroscopy"
+      actual.is_a?(Arxiv::Manuscript) && actual.title == expected_title
+    end
+  end
+
+  describe "get" do
+    it "should fetch a manuscript when passed a valid id" do
+      Arxiv.get('1202.0819').should fetch_valid_manuscript
+    end
+    it "should fetch a manuscript when passed a valid id with a version number" do
+      Arxiv.get('1202.0819v1').should fetch_valid_manuscript
+    end
+    it "should raise a manuscript not found error when the manuscript cannot be found on arXiv" do
+      lambda { Arxiv.get('1234.1234') }.should raise_error(Arxiv::Error::ManuscriptNotFound)
+    end
+    it "should raise a malformed id error when the manuscript id has an incorrect format" do
+      lambda { Arxiv.get('cond-mat0709123') }.should raise_error(Arxiv::Error::MalformedId)
+    end
+  end
+
+end
